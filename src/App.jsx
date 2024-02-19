de83fef3a34d902fba9692acc7abdb329250ea59
Prasanna Nimbalkar <prasannanimbalkar163@gmail.com>
2024-02-19 18:29:59 -0500

    adding-intial-setup

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Landing from './pages/Landing';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import PageNotFound from './pages/PageNotFound';
+import Header from './components/Header';
+import Contact from './pages/Contact';
+import ScrollToTop from './components/ScrollToTop';
+import Footer from './components/Footer';
+
+function App() {
+
+  return (
+    <Router>
+      
+        <>
+          <Header />
+          <ScrollToTop />
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/*" element={<PageNotFound />} />
+          </Routes>
+          <Footer />
+        </>
+      
+    </Router>
+  )
+}
+
+export default App
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,11 @@
+
+const Landing = () => {
+  return (
+      <div>
+          <h1>Welcome to my website</h1>
+          <h6>Site is Still under construction </h6>
+    </div>
+  )
+}
+
+export default Landing
