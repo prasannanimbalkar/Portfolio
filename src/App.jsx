fca703158135f5dfa5b8b668ca9a3968784b2ebb
Prasanna Nimbalkar <prasannanimbalkar163@gmail.com>
2024-02-19 19:21:16 -0500

    updating-path

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,11 @@ function App() {
           <ScrollToTop />
           <Routes>
             <Route path="/" element={<Landing />} />
+            <Route path="/Portfolio" element={<Landing />} />
             <Route path="/about" element={<About />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="/*" element={<PageNotFound />} />
+            <Route path="*" element={<PageNotFound />} />
           </Routes>
           <Footer />
         </>
